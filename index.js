eeacc3dc6d70b9ecc1145ef216f665a811db1e7c
Diuda <divyanshu.1996@gmail.com>
2018-12-12 01:17:39 +0530

    Help Intent Added

--- index.js
+++ index.js
@@ -38,5 +38,20 @@ const GithubRepoBranch = {
     }
 }
 
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+            && handlerInput.requestEnvelope.request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speechText = 'You can say hello to me!';
+        return handlerInput.responseBuilder
+            .speak(speechText)
+            .reprompt(speechText)
+            .withSimpleCard('Hello World', speechText)
+            .getResponse();
+    }
+};
+
 
 
